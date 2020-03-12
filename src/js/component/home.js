dbbedafdecf5c3bf1cc272a51aba9e29a1f629e7
Bienvenido Quintna <BienvenidoQuintana34@gmail.com>
2020-03-12 15:39:45 +0000

    update

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -8,7 +8,9 @@ let va = [
 	"https://media.nbcnewyork.com/2019/09/tlmd_7_train_nyc.jpg?fit=650%2C366",
 	"https://s3-prod.crainsnewyork.com/s3fs-public/BLOGS04_160519867_AR_-1_STFYVAUTRXQG.jpg",
 	"https://brooklynreporter.com/wp-content/uploads/2015/07/IMG_7431-624x416.jpg",
-	"https://i.ytimg.com/vi/b9ndnArkPp8/maxresdefault.jpg"
+    "https://i.ytimg.com/vi/b9ndnArkPp8/maxresdefault.jpg",
+    "https://pbs.twimg.com/media/DTX1FxTXUAAyX--.jpg",
+    "https://upload.wikimedia.org/wikipedia/commons/7/70/Miami_Metrorail_Hitachi_train_20190117.jpg"
 ];
 
 export function Home() {
