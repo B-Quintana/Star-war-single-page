c88a95fb2f70d639cf78cb53d995f2701609820f
Bienvenido Quintna <BienvenidoQuintana34@gmail.com>
2020-03-23 15:16:25 +0000

    update

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -5,21 +5,21 @@ import { Jumbotron } from "./jumbotron";
 let list = [
   {
     title: 'NYC Train',
-    link:  "https://s.abcnews.com/images/US/subway-1-gty-file-ml-190524_hpMain_4x3_992.jpg",
+    link:  'https://s.abcnews.com/images/US/subway-1-gty-file-ml-190524_hpMain_4x3_992.jpg',
     description: 'Inwoods',
-    imgUrl: "https://s.abcnews.com/images/US/subway-1-gty-file-ml-190524_hpMain_4x3_992.jpg"
+    imgUrl: 'https://s.abcnews.com/images/US/subway-1-gty-file-ml-190524_hpMain_4x3_992.jpg'
   },
   {
     title: 'Cool Train',
-    link:   "https://s3-prod.crainsnewyork.com/s3fs-public/BLOGS04_160519867_AR_-1_STFYVAUTRXQG.jpg",
+    link:   'https://s3-prod.crainsnewyork.com/s3fs-public/BLOGS04_160519867_AR_-1_STFYVAUTRXQG.jpg',
     description: 'This train was created in 1919',
-    imgUrl: "https://s3-prod.crainsnewyork.com/s3fs-public/BLOGS04_160519867_AR_-1_STFYVAUTRXQG.jpg"
+    imgUrl: 'https://s3-prod.crainsnewyork.com/s3fs-public/BLOGS04_160519867_AR_-1_STFYVAUTRXQG.jpg'
   },
   {
     title: 'White Bullter Train',
-    link:  "https://upload.wikimedia.org/wikipedia/commons/7/70/Miami_Metrorail_Hitachi_train_20190117.jpg",
+    link:  'https://upload.wikimedia.org/wikipedia/commons/7/70/Miami_Metrorail_Hitachi_train_20190117.jpg',
     description: 'Fast train',
-    imgUrl:  "https://upload.wikimedia.org/wikipedia/commons/7/70/Miami_Metrorail_Hitachi_train_20190117.jpg"
+    imgUrl:  'https://upload.wikimedia.org/wikipedia/commons/7/70/Miami_Metrorail_Hitachi_train_20190117.jpg'
   }
 ]
 
