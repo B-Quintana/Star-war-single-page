a5a876ed9797f4b9250b5f4d810860098bc2db06
Bienvenido Quintna <BienvenidoQuintana34@gmail.com>
2020-05-08 20:12:36 +0000

    update

--- src/js/component/cards.js
+++ src/js/component/cards.js
@@ -5,18 +5,17 @@ export function Cards(props) {
 	return (
 		<div className="m-3 text-center" style={{ width: "300px" }}>
 			<img
-				src= {props.myimage.title};
-                      {props.myimage.link};
-                    {props.myimage.description};
-                       {props.myimage.imgUrl}
+				src=   {props.myimage.imgUrl}
 				className="card-img-top"
 				alt="Card image cap"
 				style={{ height: "300px", width: "auto", maxWidth: "300px" }}
 			/>
 			<div className="card-body">
-				<h5 className="NYC Trains">NYC Trains</h5>
-				<p className="card-text">NYC Trains</p>
-				<a href="#" className="btn btn-primary">
+				<h5 classNamegi="NYC">NYC</h5>
+				<p className="card-text">{props.myimage.title}
+				
+				<p className={props.myimage.description}
+				<a href="#" className={props.myimage.link}
 					Go Somewhere
 				</a>
 			</div>
